d15d2bb26a5df35e4a1a9a1ba8b24b06a11e2fb2
Neonsy <neonsy@users.noreply.github.com>
2024-08-05 01:17:21 +0200

    Added delete functionality

--- src/components/TodoList/TodoItem/TodoItem.jsx
+++ src/components/TodoList/TodoItem/TodoItem.jsx
@@ -1,11 +1,17 @@
 import styles from './TodoItem.module.css';
 
-export default function TodoItem({ todo }) {
+export default function TodoItem({ todo, todos, setTodos }) {
+    function handleDelete(todo) {
+        setTodos(todos.filter((item) => item !== todo));
+    }
+
     return (
         <>
             <li className={styles.item}>
                 <span className={styles.text}>{todo}</span>
-                <button className={styles.deleteButton}>X</button>
+                <button className={styles.deleteButton} onClick={() => handleDelete(todo)}>
+                    X
+                </button>
             </li>
         </>
     );
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,12 +1,12 @@
 import TodoItem from './TodoItem/TodoItem';
 import styles from './TodoList.module.css';
 
-export default function TodoList({ todos }) {
+export default function TodoList({ todos, setTodos }) {
     return (
         <>
             <ul className={styles.list}>
                 {todos.map((todo) => (
-                    <TodoItem key={todo} todo={todo} />
+                    <TodoItem key={todo} todo={todo} todos={todos} setTodos={setTodos} />
                 ))}
             </ul>
         </>
