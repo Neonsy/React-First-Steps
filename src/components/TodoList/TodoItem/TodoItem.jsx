cfa89305aa8726b44f3473351af08edeea068714
Neonsy <neonsy@users.noreply.github.com>
2024-08-05 01:06:55 +0200

    Added remove button

--- src/components/TodoList/TodoItem/TodoItem.jsx
+++ src/components/TodoList/TodoItem/TodoItem.jsx
@@ -3,7 +3,10 @@ import styles from './TodoItem.module.css';
 export default function TodoItem({ todo }) {
     return (
         <>
-            <li className={styles.item}>{todo}</li>
+            <li className={styles.item}>
+                <span className={styles.text}>{todo}</span>
+                <button className={styles.deleteButton}>X</button>
+            </li>
         </>
     );
 }
