13e8efb446830bd1020ca1f50317d1b5665979d9
Neonsy <neonsy@users.noreply.github.com>
2024-08-05 00:00:53 +0200

    Form styling

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import styles from './Form.module.css';
+
+export default function Form({ todos, setTodos }) {
+    const [todo, setTodo] = useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        setTodos([...todos, todo]);
+        setTodo('');
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit} className={styles.form}>
+                <input type='text' value={todo} onChange={(e) => setTodo(e.target.value)} className={styles.input} />
+                <button type='submit' className={styles.button}>
+                    Add
+                </button>
+            </form>
+        </>
+    );
+}
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Form from './Form';
+import Form from './Form/Form';
 import TodoList from './TodoList';
 
 export default function Todo() {
